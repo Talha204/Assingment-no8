ff78433aab4fcb1a3dd8dc5148b2840ac1976ea3
Talha Rajpoot <rt3830279@gmail.com>
2023-09-12 00:16:21 +0500

    .

--- reactjs/class01/src/App.js
+++ reactjs/class01/src/App.js
@@ -12,8 +12,9 @@ function App() {
     <Header/>
       <Header1/>
       <h1>this is a first heading</h1>
-      <h2> This is second heading</h2>
+      
      <p>you are the best</p>
+     
      <Footer/>
     </div>
   );
